fa6c1f08b9b9743adb94dcb09b6308ca9f960572
samson <ksamy90@aol.com>
2017-02-26 19:54:34 +0000

    Modify Serverjs file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 
-//Create our app
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Express Server Has Started!");
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
